58a41a0952dab476356c5d6e501f8af88bfee09b
Giulianobrjf <giuliano.leite2002@gmail.com>
2023-07-20 19:36:02 -0300

    Linear probing apenas para a esquerda

--- src/main/java/org/example/Linear_Probing.java
+++ src/main/java/org/example/Linear_Probing.java
@@ -3,113 +3,111 @@ package org.example;
 import java.util.Arrays;
 import java.util.List;
 
-public class Linear_Probing {
+public class Linear_Probing{
 
     private int tamanhoatual, tamanhomaximo, incremento;
     private List<String> indices;
     private List<String> valores;
 
-    public Linear_Probing(int tamanhomaximo, int incremento)
-    {
+    public Linear_Probing(int tamanhomaximo, int incremento) {
         tamanhoatual = 0;
         this.tamanhomaximo = tamanhomaximo;
-        indices = Arrays.asList(new String[tamanhomaximo]);;
-        valores = Arrays.asList(new String[tamanhomaximo]);;
+        indices = Arrays.asList(new String[tamanhomaximo]);
+        valores = Arrays.asList(new String[tamanhomaximo]);
         this.incremento = incremento;
     }
 
-
-    public void makeEmpty()
-    {
+    public void makeEmpty() {
         tamanhoatual = 0;
         indices = Arrays.asList(new String[tamanhomaximo]);
         valores = Arrays.asList(new String[tamanhomaximo]);
     }
 
+    public int getTamanho() {
+        return tamanhoatual;
+    }
 
-    public int getTamanho() { return tamanhoatual; }
-
-    public boolean isFull()
-    {
+    public boolean isFull() {
         return tamanhoatual == tamanhomaximo;
     }
 
+    public boolean isEmpty() {
+        return getTamanho() == 0;
+    }
 
-    public boolean isEmpty() { return getTamanho() == 0; }
-
-
-    public boolean contains(String valor)
-    {
+    public boolean contains(String valor) {
         return get(valor) != null;
     }
 
-    private int hash(String valor)
-    {
+    private int hash(String valor) {
         return Integer.parseInt(valor) % tamanhomaximo;
     }
-    public int incrementando(int indice){
+
+    public int incrementando(int indice) {
         int i = (int) ((indice + incremento * Math.pow(-1, incremento)) % tamanhomaximo);
-        if(i < 0){
+        if (i < 0) {
             i = tamanhomaximo + i;
         }
         return i;
     }
 
-    public void insert(String indice, String valor)
-    {
+    public int incrementandoBidirectional(int indice, int direction) {
+        int i = (int) ((indice + incremento * Math.pow(-1, direction)) % tamanhomaximo);
+        if (i < 0) {
+            i = tamanhomaximo + i;
+        }
+        return i;
+    }
+
+    public void insert(String indice, String valor) {
         int tmp = hash(indice);
         int i = tmp;
 
-        if(!isFull()){
-        while (indices.get(i) != null){
-            i = incrementando(i);
-        }
-        indices.set(i, indice);
-        valores.set(i, valor);
-        tamanhoatual++;
-        }
-        else{
+        if (!isFull()) {
+            int k = 1;
+            while (indices.get(i) != null && indices.get(i) != "A") {
+                i = incrementandoBidirectional(i,k);
+                k++;
+            }
+            indices.set(i, indice);
+            valores.set(i, valor);
+            tamanhoatual++;
+        } else {
             System.out.println("Tabela Cheia");
         }
-
     }
 
-    public String get(String indice)
-    {
+    public String get(String indice) {
         int i = hash(indice);
         while (indices.get(i) != null) {
             if (indices.get(i).equals(indice))
                 return valores.get(i);
-            i = incrementando(i);
+            i = incrementandoBidirectional(i, 1);
         }
         return null;
     }
 
-
-    public void remove(String indice)
-    {
-
+    public void remove(String indice) {
         if (!contains(indice))
             return;
 
         int i = hash(indice);
         while (!indice.equals(indices.get(i)))
-            i = incrementando(i);
-        indices.set(i,null);
-        valores.set(i,null);
+            i = incrementandoBidirectional(i, 1);
+        indices.set(i, null);
+        valores.set(i, null);
 
-        for (i = incrementando(i); indices.get(i) != null;
-             i = incrementando(i)) {
+        for (i = incrementandoBidirectional(i, 1); indices.get(i) != null; i = incrementandoBidirectional(i, 1)) {
             String tmp1 = indices.get(i), tmp2 = valores.get(i);
-            indices.set(i,null);
-            valores.set(i,null);
+            indices.set(i, null);
+            valores.set(i, null);
             tamanhoatual--;
             insert(tmp1, tmp2);
         }
         tamanhoatual--;
     }
-    public void removeA(String indice){
 
+    public void removeA(String indice) {
         int i = hash(indice);
 
         while (indices.get(i) != null) {
@@ -119,27 +117,26 @@ public class Linear_Probing {
                 tamanhoatual--;
                 break;
             }
-            i = incrementando(i);
+            i = incrementandoBidirectional(i, 1);
         }
     }
+
     public void removeAllA() {
         for (int i = 0; i < tamanhomaximo; i++) {
-            if(valores.get(i) == "A"){
+            if (valores.get(i) == "A") {
                 System.out.println(i);
-                valores.set(i,null);
-                indices.set(i,null);
+                valores.set(i, null);
+                indices.set(i, null);
                 remove(String.valueOf(i));
             }
         }
     }
 
-
-    public void printHashTable()
-    {
+    public void printHashTable() {
         System.out.println("\nHash Table: ");
         for (int i = 0; i < tamanhomaximo; i++)
             if (indices.get(i) != null)
-                System.out.println(indices.get(i) + " " + valores.get(i));
+                System.out.println(i+" : "+indices.get(i) + " " + valores.get(i));
         System.out.println();
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,113 +4,84 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
-                Scanner scan = new Scanner(System.in);
+        System.out.println("Hash Table Test\n\n");
+        System.out.println("Digite o tamanho máximo da tabela:");
+        int tamanhomaximo = scan.nextInt();
+        System.out.println("Digite o incremento:");
+        int incremento = scan.nextInt();
 
-                System.out.println("Hash Table Test\n\n");
-                System.out.println("Digite uma entrada");
+        Linear_Probing lpht = new Linear_Probing(tamanhomaximo, incremento);
 
-                Linear_Probing lpht = new Linear_Probing(scan.nextInt(),3);
 
-                lpht.insert("12","20");
-                lpht.printHashTable();
-                lpht.insert("5","22");
-                lpht.printHashTable();
-                lpht.insert("15","4");
-                lpht.printHashTable();
-                lpht.insert("19","13");
-                lpht.printHashTable();
+        lpht.insert("1", "A");
+        lpht.insert("24", "B");
+        lpht.insert("2", "C");
+        lpht.insert("47", "D");
+        lpht.insert("70", "E");
+        lpht.insert("10", "F");
+        lpht.insert("33", "G");
+        lpht.insert("20", "I");
+
+        lpht.printHashTable();
         char ch;
 
-                do
-                {
-                   // Menu
-                    System.out.println("\nHash Table Operações\n");
-                    System.out.println("1. Inserir ");
-                    System.out.println("2. Remover");
-                    System.out.println("3. Procurar");
-                    System.out.println("4. Limpar");
-                    System.out.println("5. Tamanho");
-                    System.out.println("6. Define Avaliable");
-                    System.out.println("7. Remove todos Avaliable");
-                    int choice = scan.nextInt();
+        do {
+            // Menu
+            System.out.println("\nHash Table Operações\n");
+            System.out.println("1. Inserir ");
+            System.out.println("2. Remover");
+            System.out.println("3. Procurar");
+            System.out.println("4. Limpar");
+            System.out.println("5. Tamanho");
+            System.out.println("6. Define Avaliable");
+            System.out.println("7. Remove todos Avaliable");
+            int choice = scan.nextInt();
 
+            switch (choice) {
+                case 1:
+                    System.out.println("Digite um índice e um valor:");
+                    lpht.insert(scan.next(), scan.next());
+                    break;
 
-                    switch (choice) {
+                case 2:
+                    System.out.println("Digite um índice:");
+                    lpht.remove(scan.next());
+                    break;
 
-                        // Case 1
-                        case 1:
+                case 3:
+                    System.out.println("Digite um indice:");
+                    System.out.println("Valor = " + lpht.get(scan.next()));
+                    break;
 
+                case 4:
+                    lpht.makeEmpty();
+                    System.out.println("Hash Table Limpa\n");
+                    break;
 
-                            System.out.println("Digite um indice e um valor");
-                            lpht.insert(scan.next(), scan.next());
+                case 5:
+                    System.out.println("Tamanho = " + lpht.getTamanho());
+                    break;
 
-                            break;
+                case 6:
+                    System.out.println("Digite um índice:");
+                    lpht.removeA(scan.next());
+                    break;
 
+                case 7:
+                    lpht.removeAllA();
+                    break;
 
-                        case 2:
-
-
-                            System.out.println("Digite um indice");
-                            lpht.remove(scan.next());
-
-                            break;
-
-
-                        case 3:
-
-
-                            System.out.println("Digite um valor");
-                            System.out.println("Valor = "
-                                    + lpht.get(scan.next()));
-
-                            break;
-
-
-                        case 4:
-
-                            lpht.makeEmpty();
-
-                            System.out.println("Hash Table Limpa\n");
-
-                            break;
-
-
-                        case 5:
-
-                            System.out.println("Tamanho = " + lpht.getTamanho());
-                            break;
-                        case 6:
-
-                            System.out.println("Digite um indice");
-                            lpht.removeA(scan.next());
-
-
-                            break;
-                        case 7:
-
-                            lpht.removeAllA();
-
-
-                            break;
-
-                        default:
-
-                            System.out.println("Entrada errada \n ");
-
-                            break;
-                    }
-
-
-                    lpht.printHashTable();
-
-
-                    System.out.println(
-                            "\n Deseja continuar (Digite y ou n)\n");
-
-                    ch = scan.next().charAt(0);
-                } while (ch == 'Y' || ch == 'y');
-
-
+                default:
+                    System.out.println("Entrada inválida.\n");
+                    break;
             }
-        }
+
+            lpht.printHashTable();
+
+            System.out.println("\nDeseja continuar (Digite y ou n):");
+            ch = scan.next().charAt(0);
+        } while (ch == 'y' || ch == 'Y');
+    }
+}
